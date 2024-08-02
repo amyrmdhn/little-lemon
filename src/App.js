6cc8813a5afbab602a60ede2ed9791aef7c078ae
amyrmdhn <amyramadhan99@gmail.com>
2024-08-02 16:46:01 +0700

    Finished v1 basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="App">
-      <p>Test</p>
-    </div>
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
   );
 }
 
